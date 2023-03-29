6b3f898eff794a20911733b70d31d9a0de8a58f6
nehabirajdar <nsb9823@gmail.com>
2023-03-29 13:03:45 +0530

    Add project files.

---
+++ BankEvent/Bank.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEvent
+{
+    public delegate void MyDelegate();
+    public class Bank
+    {
+        public event MyDelegate NoBalance;
+        public event MyDelegate LowBalance;
+
+
+        private double balance;
+        public Bank(double balance)
+        {
+            this.balance = balance;
+        }
+        public void Credit()
+        {
+            double balance = double.Parse(Console.ReadLine());
+            Console.WriteLine(balance);
+
+
+        }
+        public void Debit(double debitamt)
+        {
+
+
+            if (debitamt > balance)
+            {
+                NoBalance();
+
+            }
+            if ((balance - debitamt) < 3000)
+            {
+                LowBalance();
+
+            }
+
+
+        }
+    }
+        public class Program
+        {
+           
+            static void Message2()
+            {
+                Console.WriteLine("No balance");
+            }
+            static void Message3()
+            {
+                Console.WriteLine("Low balance");
+
+            }
+            static void Main(string[] args)
+            {
+                Bank bank = new Bank(1000);
+
+                bank.NoBalance += new MyDelegate(Message2);
+                bank.LowBalance += new MyDelegate(Message3);
+
+                bank.Credit();
+                bank.Debit(500);
+            }
+        }
+    }
---
+++ EventName/Name.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventName
+{
+    public delegate void MyDelegate();
+    public class Name
+    {
+        public event MyDelegate NameRequired;
+        public event MyDelegate NameValid;
+
+        public void accept(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                NameRequired();
+            }
+            else
+            {
+                NameValid();
+            }
+        }
+
+        public class Progarm
+        {
+            static void Message1()
+            {
+                Console.WriteLine("Name is required");
+            }
+            static void Message2()
+            {
+                Console.WriteLine("Name is valid");
+            }
+
+            static void Main(string[] args)
+            {
+
+                Name n = new Name();
+                n.NameRequired += new MyDelegate(Message1);
+                n.NameValid += new MyDelegate(Message2);
+                n.accept("");
+
+            }
+        }
+    }
+}
